4c92b92406fd34ff0ffcca9100b32423035d7714
root root <root@localhost>
2023-11-25 09:41:36 +0200

    messing with the cookies

--- ClipsMaker/main.py
+++ ClipsMaker/main.py
@@ -6,21 +6,17 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
 
-driver.get("http://example.com/some404page")
+driver.get("https://www.twitch.tv/videos/1978136480")
 # driver.add_cookie({
 #     "name": "key", 
 #     "value": "value"})
+driver.delete_all_cookies()
 
+# driver.add_cookie(auth_token_cookie)
+
+assert "Twitch" in driver.title
 driver.add_cookie(api_token_cookie)
 driver.add_cookie(auth_token_cookie)
 driver.add_cookie(login_cookie)
 driver.add_cookie(name_cookie)
-# driver.add_cookie(auth_token_cookie)
-
-assert "Twitch" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
 # driver.close()
