55a8e3b452fc9ac1fbf5417e1cdc2fdf25bb56ea
root root <root@localhost>
2023-11-25 09:18:37 +0200

    lol. git ignore

--- ClipsMaker/main.py
+++ ClipsMaker/main.py
@@ -4,11 +4,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
+
+driver.add_cookie({"name": "key", "value": "value"})
+
+driver.get("https://www.twitch.tv/videos/1978136480")
+assert "Twitch" in driver.title
 elem = driver.find_element(By.NAME, "q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
-driver.close()
+# driver.close()
